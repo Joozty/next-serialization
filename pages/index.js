c392a7667c1660311a326390fd90e976343af97e
Jozef Harag <32jojo32@gmail.com>
2020-08-12 17:49:43 +0200

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+export async function getServerSideProps(context) {
+  return {
+    props: {
+        longList: Array.from(Array(10000), (_, i) => ({
+            id: i,
+            name: 'test name'
+        })),
+    },
+  }
+}
+
+export function reportWebVitals(metric) {
+    console.log(metric)
+}
+
+export default function Home({longList}) {
+  return <h1>Test {longList.length}</h1>
+}
